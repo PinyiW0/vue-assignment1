541c8fa47839b6d464bd9942ef09a26f355493ff
Pinyi Wu <wpypy8@gmail.com>
2025-08-31 20:42:42 +0800

    chore: 更改設定檔版本

---
+++ src/views/WeekThree.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// 關於頁面元件邏輯
+</script>
+
+<template>
+  <div class="">
+    <h1>關於我們</h1>
+    <p>這是關於我們的頁面。</p>
+    <p>我們是一個專業的開發團隊，致力於建立優質的網路應用程式。</p>
+    <nav>
+      <router-link to="/" class="nav-link">首頁</router-link>
+      <router-link to="/contact" class="nav-link">聯絡我們</router-link>
+    </nav>
+  </div>
+</template>
